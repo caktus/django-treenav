3171fe92481ce6612111b1f3254de93bf52db2bc
Michael Clemmons <mike@caktusgroup.com>
2010-01-25 21:15:06 +0000

    Added exception to context processor.
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@20 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

--- treenav/context_processors.py
+++ treenav/context_processors.py
@@ -6,6 +6,9 @@ def treenav_active(request):
     for menu in menus:
         root = menu.to_tree()
         active_leaf = root.set_active(request.META['PATH_INFO'])
-        treenav_active[menu.slug] = active_leaf.get_active_items()
+        try:
+            treenav_active[menu.slug] = active_leaf.get_active_items()
+        except AttributeError:
+            return { }
     return {'treenav_active': treenav_active }
      
