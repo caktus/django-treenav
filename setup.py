16188882ca1567680e867e9843313b6648e96b24
Michael Clemmons <mike@caktusgroup.com>
2010-03-11 21:38:06 +0000

    Added initial setup with README in wiki not rest
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@25 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+packages = find_packages()
+packages += [
+    'treenav.templates',
+    'treenav.templates.treenav',
+]
+setup(
+    name='django-treenav',
+    version='0.0.0',
+    author='Caktus Consulting Group',
+    author_email='solutions@caktusgroup.com',
+    packages=find_packages(),
+    install_requires = ['Django >= 1.1,==dev',],
+    include_package_data = True,
+    exclude_package_data={
+        '': ['*.sql', '*.pyc',],
+        'treenav': ['media/*',]
+    },
+    url='http://code.google.com/p/django-treenav/',
+    license='LICENSE.txt',
+    description='Extensible, hierarchical, and pluggable navigation system for Django sites.',
+    long_description=open('README.txt').read(),
+)
