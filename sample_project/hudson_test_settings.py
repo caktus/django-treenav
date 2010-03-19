2b9c74861012ff42b98bf6901440072fae304138
Michael Clemmons <mike@caktusgroup.com>
2010-03-19 22:26:27 +0000

    Added sample project
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@33 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

---
+++ sample_project/hudson_test_settings.py
@@ -0,0 +1,6 @@
+from sample_project.settings import *
+
+TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.run_tests'
+TEST_OUTPUT_VERBOSE = True
+TEST_OUTPUT_DESCRIPTIONS = True
+TEST_OUTPUT_DIR = 'xmlrunner'
