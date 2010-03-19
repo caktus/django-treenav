ee9bba2e1d4130982458a8550396adc81b4a1a5e
Michael Clemmons <mike@caktusgroup.com>
2010-03-19 22:39:05 +0000

    Switched urls in settings for tests urls
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@34 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
 )
 
-ROOT_URLCONF = 'sample_project.urls'
+ROOT_URLCONF = 'treenav.tests.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
