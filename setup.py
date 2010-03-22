e1198d5e9de0ebe157b584686869f574f636aae0
Michael Clemmons <mike@caktusgroup.com>
2010-03-22 16:14:47 +0000

    Removed sample_project from install
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@36 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import os
 from setuptools import setup, find_packages
 
+packages = find_packages()
+packages.remove('sample_project')
+
 setup(
     name='django-treenav',
     version='0.0.0',
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
-    packages=find_packages(),
+    packages=packages,
     install_requires = ['Django >= 1.1,==dev',],
     include_package_data = True,
     exclude_package_data={
