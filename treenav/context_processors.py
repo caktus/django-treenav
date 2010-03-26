7877cadb5fc5017931357c76af673c19c32276f9
Tobias Mcnulty <tobias@caktusgroup.com>
2010-03-26 01:13:47 +0000

    support multiple top level menu items
    git-svn-id: http://django-treenav.googlecode.com/svn/trunk@38 20c0abe6-6650-11de-bf6d-a7b22aa6cdc8

--- treenav/context_processors.py
+++ treenav/context_processors.py
@@ -1,14 +1,12 @@
 from treenav.models import MenuItem
 
+
 def treenav_active(request):
     menus = MenuItem.objects.filter(parent__isnull=True).all()
     treenav_active = {}
     for menu in menus:
         root = menu.to_tree()
         active_leaf = root.set_active(request.META['PATH_INFO'])
-        try:
+        if active_leaf:
             treenav_active[menu.slug] = active_leaf.get_active_items()
-        except AttributeError:
-            return { }
-    return {'treenav_active': treenav_active }
-     
+    return {'treenav_active': treenav_active}
