e368ce5b342e56fc6c9f623342933e6583154e4e
Colin Copeland <copelco@caktusgroup.com>
2010-09-18 18:11:04 -0400

    add pypi info

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-import os
 from setuptools import setup, find_packages
-
-packages = find_packages()
-packages.remove('sample_project')
 
 setup(
     name='django-treenav',
-    version='0.0.0',
+    version=__import__('treenav').__version__,
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
-    packages=packages,
-    install_requires = [],
-    include_package_data = True,
-    exclude_package_data={
-        '': ['*.sql', '*.pyc',],
-        'treenav': ['media/*',]
-    },
-    url='http://code.google.com/p/django-treenav/',
+    packages=find_packages(exclude=['sample_project']),
+    include_package_data=True,
+    url='http://http://github.com/caktus/django-treenav',
     license='LICENSE.txt',
-    description='Extensible, hierarchical, and pluggable navigation system for Django sites.',
+    description='Extensible, hierarchical, and pluggable navigation system '
+                'for Django sites',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
     long_description=open('README.rst').read(),
 )
