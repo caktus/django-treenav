89717e929b3a07f2e0ee8dd2c4183eeda33315f0
Tobias McNulty <tobias@caktusgroup.com>
2011-03-13 14:38:56 -0400

    fix sample project URLs and enable admin interface

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Example:
@@ -13,6 +13,6 @@ urlpatterns = patterns('',
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-    (r'^%streenav/' % settings.URL_PREFIX, include('treenav.urls.admin')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^treenav/', include('treenav.urls.admin')),
 )
