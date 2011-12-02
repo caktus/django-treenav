ed29d7e32d59eb0c3a2360424d530d7c298dd5d2
Colin Copeland <copelco@caktusgroup.com>
2011-12-02 14:58:32 -0500

    provide more order choices by default

--- treenav/models.py
+++ treenav/models.py
@@ -91,19 +91,7 @@ class MenuItemManager(models.Manager):
 
     
 class MenuItem(models.Model):
-    ORDER_CHOICES = (
-        (0, 0),
-        (1, 1),
-        (2, 2),
-        (3, 3),
-        (4, 4),
-        (5, 5),
-        (6, 6),
-        (7, 7),
-        (8, 8),
-        (9, 9),
-    )
-    
+
     parent = models.ForeignKey(
         'MenuItem',
         related_name='children',
@@ -123,7 +111,7 @@ class MenuItem(models.Model):
     )
     order = models.IntegerField(
         _('order'),
-        choices=ORDER_CHOICES,
+        choices=[(x, x) for x in xrange(0, 51)],
     )
     is_enabled = models.BooleanField(default=True)
     link = models.CharField(
