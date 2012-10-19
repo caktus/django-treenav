0f0d8f2e346fb92a37fceca4bc3abf600d7a8831
Mark Lavin <mlavin@caktusgroup.com>
2012-10-19 17:08:41 -0400

    Reorganize tests and setup a 404 handler so that it won't look for a 404.html.

---
+++ treenav/tests/test_models.py
@@ -0,0 +1,35 @@
+from django.test import TransactionTestCase
+
+from treenav.models import MenuItem
+
+
+class TreeOrder(TransactionTestCase):
+    """
+    Test checking that if children added out of order to a new tree will be
+    ordered properly when called.
+    The original source of this bug is django-mptt but this does check that menu
+    item is ordering by the correct attributes as well.
+    https://github.com/django-mptt/django-mptt/issues#issue/14
+    """
+
+    def test_order(self):
+        primary_nav = MenuItem(
+            label='primary-nav',
+            slug='primary-nav',
+            order=0,
+            )
+        primary_nav.save()
+        child = {}
+        for i in [2,4,5,1,0,8]:
+            child[i] = MenuItem(
+                parent=primary_nav,
+                label=str(i),
+                slug=str(i),
+                order=i,
+                link='/',
+                )
+            child[i].save()
+        order = MenuItem.objects.exclude(
+            slug='primary-nav'
+            ).values_list('order', flat=True)
+        self.assertEquals(list(order), sorted(order))
--- treenav/tests/urls.py
+++ treenav/tests/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls.defaults import *
-from django.http import HttpResponse, HttpRequest
+from django.conf.urls.defaults import url, patterns, include, handler404
+from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 from django.template import Template, Context
+
 
 def test_view(request, item_slug):
     pslug = request.POST['pslug']
@@ -13,11 +14,15 @@ def test_view(request, item_slug):
     })
     return HttpResponse(t.render(c))
 
-urlpatterns = patterns(
-    '',
-    url(r'^item/(?P<item_slug>[\w-]+)/$',
-        test_view, 
-        name='test_view',
-    ),
-    (r'^old/', include('treenav.urls.undefined_url')),
+
+def test_404(request):
+    return HttpResponseNotFound()
+
+
+handler404 = test_404
+
+
+urlpatterns = patterns('',
+    url(r'^item/(?P<item_slug>[\w-]+)/$', test_view, name='test_view'),
+    url(r'^old/', include('treenav.urls.undefined_url')),
 )
