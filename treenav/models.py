0238764e7e3f10c25398c2fee0dbb5994fdb0e28
Vinod Kurup <vinod@kurup.com>
2014-08-24 21:30:34 -0400

    Change object_id to a CharField to allow linking to UUID PKs

--- treenav/models.py
+++ treenav/models.py
@@ -119,9 +119,11 @@ class MenuItem(MPTTModel):
         null=True,
         blank=True,
     )
-    object_id = models.PositiveIntegerField(
-        null=True,
+    object_id = models.CharField(
+        # use a CharField to be able to point to tables with UUID pks
+        max_length=36,
         blank=True,
+        db_index=True,
     )
     content_object = generic.GenericForeignKey('content_type', 'object_id')
     href = models.CharField(_('href'), editable=False, max_length=255)
