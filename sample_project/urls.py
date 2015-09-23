dcd117482eacc1187e1d001d95108017585d3092
bphillips <bphillips@caktusgroup.com>
2015-09-23 14:07:39 -0400

    Updated sample_project urlconf to use a list instead of deprecated patterns and to import from the proper module.

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',  # noqa
+urlpatterns = [  # noqa
     url(r'^admin/', include(admin.site.urls)),
     url(r'^treenav/', include('treenav.urls')),
     # Catch all URL to easily demonstrate treenav display
     url(r'^', TemplateView.as_view(template_name='base.html')),
-)
+]
