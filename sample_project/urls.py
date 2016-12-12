9767503687af676185bf95152fd38c643bf39511
Vinod Kurup <vinod@kurup.com>
2016-12-12 12:10:51 -0500

    Support Django 1.10

--- sample_project/urls.py
+++ sample_project/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = [  # noqa
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^treenav/', include('treenav.urls')),
     # Catch all URL to easily demonstrate treenav display
